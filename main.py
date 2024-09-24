3ec9dff511129973fee0660f6c8434aebe158d8e
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-09-24 11:10:51 -0600

    Add fn to get subnet ids

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 import boto3
 
+ec2_client = boto3.client('ec2')
+
+all_subnets = ec2_client.describe_subnets()['Subnets']
+
+def get_all_subnets():
+    for subnet in all_subnets:
+        print(subnet['SubnetId'])
+
