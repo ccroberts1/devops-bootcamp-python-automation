0e9a8b723e93e18dcb0975d6e1bb7c3b97cbff67
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-09-26 17:02:53 -0600

    Resetting values to troubleshoot issue with tag list

--- jenkins/get-images.py
+++ jenkins/get-images.py
@@ -2,14 +2,14 @@ import boto3
 import os
 
 repo_name = os.environ['ECR_REPO_NAME']
+
 ecr_client = boto3.client('ecr')
 
-images_list = ecr_client.describe_images(repositoryName=repo_name)['imageDetails']
+images = ecr_client.describe_images(repositoryName=repo_name)
 
-image_tags_list = []
-for image in images_list:
-    print(image)
-    image_tags_list.append(image['imageTags'][0])
+image_tags = []
+for image in images['imageDetails']:
+    image_tags.append(image['imageTags'][0])
 
-for tag in image_tags_list:
+for tag in image_tags:
     print(tag)
