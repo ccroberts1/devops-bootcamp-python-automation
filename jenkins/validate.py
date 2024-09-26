c749737dd35ec201dafcb099485760d035ba51e9
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-09-26 11:04:38 -0600

    Add scripts for deploying and validating app

---
+++ jenkins/validate.py
@@ -0,0 +1,17 @@
+import requests
+import time
+import os
+
+ssh_host = os.environ['EC2_SERVER']
+host_port = os.environ['HOST_PORT']
+
+try:
+    time.sleep(15)
+
+    response = requests.get(f"http://{ssh_host}:{host_port}")
+    if response.status_code == 200:
+        print("Application is running!")
+    else:
+        print("Application was not deployed successfully")
+except Exception:
+    print(f"Connection error: {Exception}")
