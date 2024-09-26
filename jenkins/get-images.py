72dc78f09a0d64164772b7f99572b0627d70c84e
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2024-09-26 12:42:53 -0600

    Fix issue with images_list response in get-images

--- jenkins/get-images.py
+++ jenkins/get-images.py
@@ -5,11 +5,12 @@ repo_name = os.environ['ECR_REPO_NAME']
 
 ecr_client = boto3.client('ecr')
 
-images_list = ecr_client.describe_images(repositoryName=repo_name)
+images_list = ecr_client.describe_images(repositoryName=repo_name)['imageDetails']
 
 image_tags_list = []
 for image in images_list:
-    image_tags_list.append(image['imageTag'][0])
+    print(image)
+    image_tags_list.append(image['imageTags'][0])
 
 for tag in image_tags_list:
     print(tag)
